52dfdbf6d4265c9d5d4799879f4c707a20bb4d03
Andy Kohler <andykohler99@gmail.com>
2019-11-19 16:19:12 -0500

    First commit

---
+++ MasterMind/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Linq;
+
+namespace MasterMind
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        
+        {
+
+            Console.WriteLine("Welcome to the Guessing Game! You are guessing a " +
+              "4 digit number with digits no lower than 1 and no higher than 6. You have ten tries. " +
+              "Press any key to continue.");
+
+            Console.ReadKey();
+
+            Console.WriteLine("Type a 4 digit number, with no digit lower than 1 or higher than 6.");
+            string guess = Console.ReadLine();
+
+            Convert.ToInt32(guess);
+
+           
+
+            
+
+            var rand = new Random();
+
+            int Min = 1;
+            int Max = 6;
+            Random randNum = new Random();
+            int[] First = Enumerable
+                .Repeat(1, 6)
+                .Select(i => randNum.Next(Min, Max))
+                .ToArray();
+
+            if (First[0] =)
+            {
+
+            }
+
+  
+           
+
+
+
+            
+
+
+
+
+
+
+
+            //int[] numberString = new int[4];
+            //numberString[0] = 3;
+            //numberString[1] = 6;
+            //numberString[2] = 2;
+            //numberString[3] = 1;
+
+
+         
+        }
+
+    }
+}
