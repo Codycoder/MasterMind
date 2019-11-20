94bd424c430ff9db24c835ed88986b6357760fe5
Andy Kohler <andykohler99@gmail.com>
2019-11-19 22:07:56 -0500

    headway with integrating arrays/lists in place of single digits

--- MasterMind/Program.cs
+++ MasterMind/Program.cs
@@ -11,106 +11,42 @@ namespace MasterMind
         {
             var rand = new Random();
             int answerTwo;
-            int randomNumber = new int();
-
+            //int randomNumber;
+            //int[] userArray;
+            //int[] computerArray;
 
             //Console.WriteLine("1 random integer generated");
-            for (int ctr = 0; ctr <= 0; ctr++)
-            //Console.Write("{0,8:N0}",
-            randomNumber = rand.Next(1, 6);
+            for (int ctr = 0; ctr <= 1; ctr++)
+                Console.WriteLine("{0,8:N0}", rand.Next(1, 6));
 
-            Console.WriteLine("Choose a number between 1 and 6.");
+            for (int ctr = 0; ctr <= 2; ctr++)
+            {
+                int[] computerArray = new int[2];
+            }
+
+            Console.WriteLine("Guess a two digit number with digits between 1 and 6.");
             answerTwo = Convert.ToInt32(Console.ReadLine());
-
-            if (answerTwo == randomNumber)
+            for (int ctr = 0; ctr <= 2; ctr++)
             {
-                Console.WriteLine("You got it!");
+                int[] userArray = new int[2];
+
+                if (userArray[0] == computerArray[0])
+                {
+                    Console.Write("+");
+                }
+                else if (userArray[0] == computerArray[1])
+                {
+                    Console.Write("-");
+                }
+                else if (userArray[0] != computerArray[0])
+                {
+                    Console.WriteLine("Nope!");
+                }
+                else if (userArray[0] != computerArray[1])
+                {
+                    Console.WriteLine("Nope!");
+                }
             }
-            else
-            {
-                Console.WriteLine("Nope!");
-            }
-
-            //        {
-            //            Console.WriteLine("Looks like you nailed it!");
-            //}
-
-            //else
-            //{
-            //    Console.WriteLine("Guess again!");
-            //}
-
-
-            //bool higher = (rand = answerTwo);
-
-            //bool lower = (answerOne < answerTwo);
-
-            //bool same = (answerOne == answerTwo);
-
-            //if (higher)
-            //{
-            //    Console.WriteLine(answerOne + " is higher");
-            //}
-            //if (lower)
-            //{
-            //    Console.WriteLine(answerOne + " is lower!");
-            //}
-            //else
-            //{
-            //    Console.WriteLine("Numbers are even, I guess!");
-            //}
-
-
-
-
-
-
-
-
-
-
-
-
-            //Console.WriteLine("Welcome to the Guessing Game! You are guessing a " +
-            //             "4 digit number with digits no lower than 1 and no higher than 6. You have ten tries. " +
-            //             "Press any key to continue.");
-
-            //Console.ReadKey();
-
-            //Console.WriteLine("Type a 4 digit number, with no digit lower than 1 or higher than 6.");
-            //string guess = Console.ReadLine();
-
-            //Convert.ToInt32(guess);
-
-
-
-
-
-            //var rand = new Random();
-
-            //int Min = 1;
-            //int Max = 6;
-            //Random randNum = new Random();
-            //int[] First = Enumerable
-            //    .Repeat(1, 6)
-            //    .Select(i => randNum.Next(Min, Max))
-            //    .ToArray();
-
-            //if (First[0] =)
-            //{
-
-            //}
-
-
-
-            //int[] numberString = new int[4];
-            //numberString[0] = 3;
-            //numberString[1] = 6;
-            //numberString[2] = 2;
-            //numberString[3] = 1;
-
-
-
         }
 
     }
