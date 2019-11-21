a58807a15c62d4c7c0ae30d9ce0dc3c61d53cb0f
Andy Kohler <andykohler99@gmail.com>
2019-11-20 21:09:10 -0500

    generated 4 random numbers w/ duplicates

--- MasterMind/Program.cs
+++ MasterMind/Program.cs
@@ -12,18 +12,18 @@ namespace MasterMind
         {
             var rand = new Random();
             int answerTwo;
-            var userList = new List<int>();
-            var computerList = new List<int>();
-            int randomNumbers = new int();
+            var userList = new List<int>(4);
+            var computerList = new List<int>(4);
+            //int randomNumbers = new int();
 
             //Console.WriteLine("4 random integers generated");
-            for (int ctr = 0; ctr <= 4; ctr++)
-                randomNumbers = rand.Next(1, 6);
-                Console.WriteLine("{0,8:N0}", randomNumbers);
-                computerList.Add(randomNumbers);
+            for (int i = 0; i <= 3; i++)
+                Console.WriteLine("{0,8:N0}", rand.Next(1, 6));
+                computerList.Add(rand.Next(4));
 
             Console.WriteLine("Guess a four digit number with digits between 1 and 6.");
             answerTwo = Convert.ToInt32(Console.ReadLine());
+
             for (int ctr = 0; ctr <= 4; ctr++)
                 userList.Add(answerTwo);
 
