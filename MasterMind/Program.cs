00cb87a93b7fc77008323d92387ec341a3e04287
Andy Kohler <andykohler99@gmail.com>
2019-11-22 17:15:01 -0500

    Current iteration - loops need work

--- MasterMind/Program.cs
+++ MasterMind/Program.cs
@@ -8,42 +8,48 @@ namespace MasterMind
     {
         static void Main(string[] args)
         {
+            var rand = new Random();
+            //int userInput = new int();
+            var userList = new List<int>(4);
+            var computerList = new List<int>(4);
+            int randomNumber = new int();
+            int Turn = new int();
+
+            Console.WriteLine("Welcome to MasterMind! In this game you try to guess a 4 digit number.");
+            Console.WriteLine("You have ten tries. Press any key to continue"); 
+            Console.ReadKey();
+            Console.Clear();
+
+            for (int i = 0; i <= 3; i++)
+
+            {
+                // capture this random number for WriteLine-ing AND computerList.Add!
+                randomNumber = rand.Next(1, 6);     
+                Console.Write("{0,8:N0}", randomNumber);
+                computerList.Add(randomNumber);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("The computer is ready! Press any key to continue.");
+
+            // Printing numbers to test list population/function
+            //foreach (var item in computerList)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            Console.ReadKey();
+            Console.Clear();
+
+
             bool correctAnswer = false;
+            Turn = 0;
             do
             {
 
-                var rand = new Random();
-                //int userInput = new int();
-                var userList = new List<int>(4);
-                var computerList = new List<int>(4);
-                int randomNumber = new int();
-
-                Console.WriteLine("Welcome to MasterMind! In this game you try to guess a 4 digit number the computer has generated. " +
-                    "You have ten tries. Press any key to continue");
-                Console.ReadKey();
-                Console.Clear();
-
-                for (int i = 0; i <= 3; i++)
-
-                {
-                    randomNumber = rand.Next(1, 6);     // capture this random number for WriteLine-ing AND computerList.Add!
-                    Console.Write("{0,8:N0}", randomNumber);
-                    computerList.Add(randomNumber);
-                }
-
-                Console.WriteLine();
-                Console.WriteLine("The computer is ready! Press any key to continue.");
-
-                foreach (var item in computerList)
-                {
-                    Console.WriteLine(item);
-                }
-
-                Console.ReadKey();
-                Console.Clear();
-
                 for (int i = 0; i <= 3; i++)
                 {
+
                     Console.WriteLine("Guess a digit");
                     string userInput = Console.ReadLine();
                     userList.Add(Convert.ToInt32(userInput));
@@ -51,6 +57,7 @@ namespace MasterMind
 
                 Console.ReadKey();
 
+                // printing numbers to test list population
                 foreach (var item in userList)
                 {
                     Console.WriteLine(item);
@@ -58,16 +65,15 @@ namespace MasterMind
 
                 Console.ReadKey();
 
-                foreach (var item in userList)
-
                 {
-                    if ()
+                    if (userList[0] == computerList[0])
                     {
-                        Console.WriteLine("+");
+                        Console.WriteLine("YESSSS");
+                        break;
                     }
 
-                    else 
-                   
+                    else
+
                     {
                         Console.WriteLine("Nope, try again!");
                     }
@@ -78,12 +84,8 @@ namespace MasterMind
 
                 }
 
-            } while (correctAnswer == false);
 
-            {
-
-            }
-
+            } while (Turn <= 10 && correctAnswer == false);
         }
     }
 }
