8a76253d55a16c91e303c1374ff993193dafdc33
Andy Kohler <andykohler99@gmail.com>
2019-11-21 22:24:58 -0500

    Populated userList

--- MasterMind/Program.cs
+++ MasterMind/Program.cs
@@ -4,87 +4,109 @@ using System.Linq;
 
 namespace MasterMind
 {
-
-    class Program
+    public class Program
     {
         static void Main(string[] args)
-        
         {
+
             var rand = new Random();
-            int answerTwo;
+            //int userInput = new int();
             var userList = new List<int>(4);
             var computerList = new List<int>(4);
             int randomNumber = new int();
 
-            Console.WriteLine("Welcome to MasterMind! Try to guess a 4 digit number. You have ten tries. " +
-                "Press any key to continue");
+            Console.WriteLine("Welcome to MasterMind! In this game you try to guess a 4 digit number the computer has generated. " +
+                "You have ten tries. Press any key to continue");
             Console.ReadKey();
             Console.Clear();
 
-            //Console.WriteLine("4 random integers generated");
-            //for (int i = 0; i <= 3; i++)
-            //{
-            //    Console.Write("{0,8:N0}", rand.Next(1, 6));
-            //    computerList.Add(rand.Next(4));
-            //}
+            for (int i = 0; i <= 3; i++)
 
-            //for (int i = 0; i <= 3; i++)
-
-            //{
-            //    randomNumber = rand.Next(1, 6);     // capture this random number for WriteLine-ing AND computerList.Add!
-            //    Console.Write("{0,8:N0}", randomNumber);
-            //    computerList.Add(randomNumber);
-            //}
-
-            randomNumber = rand.Next(1, 6);     // capture this random number for WriteLine-ing AND computerList.Add!
-            Console.Write("{0,8:N0}", randomNumber);
-
+            {
+                randomNumber = rand.Next(1, 6);     // capture this random number for WriteLine-ing AND computerList.Add!
+                Console.Write("{0,8:N0}", randomNumber);
+                computerList.Add(randomNumber);
+            }
 
             Console.WriteLine();
             Console.WriteLine("The computer is ready! Press any key to continue.");
+
+            foreach (var item in computerList)
+            {
+                Console.WriteLine(item);
+            }
+
             Console.ReadKey();
             Console.Clear();
 
-            Console.WriteLine("Guess a four digit number with digits between 1 and 6.");
-            //answerTwo = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i <= 3; i++)
+            {
+            Console.WriteLine("Guess a digit");
+            string userInput = Console.ReadLine();
+            userList.Add(Convert.ToInt32(userInput));
+            }
+            
 
-            //for (int ctr = 0; ctr <= 3; ctr++)
+
+            //string guess = Console.ReadLine();
+            //int guessNumber = Convert.ToInt32(guess);
+
+            //for (int i = 0; i <= 3; i++)
             //{
-            //    userList.Add(answerTwo);
+
+            //    userList.Add(userInput);
             //}
 
-            answerTwo = Convert.ToInt32(Console.ReadLine());
-
-            if (answerTwo == randomNumber)
+            Console.ReadKey();
+            
+            foreach (var item in userList)
             {
-                Console.WriteLine("+");
+                Console.WriteLine(item);
             }
 
-            else
+            Console.ReadKey();
+
+            foreach (var item in userList)
+                //for (int i = 0; i <= 3; i++)
+
             {
-                Console.WriteLine("-");
+                if (userList[0] == computerList[0] || userList[1] == computerList[1] || userList[2] == computerList[2] || userList[3] == computerList[3])
+                {
+                    Console.Write("+");
+                }
+                else if (userList.Contains(computerList[0]))
+                {
+                    Console.Write("-");
+                }
             }
 
-            //if (userList[1] == computerList[1])
+            //foreach (var item in userInput)
+            //    Console.WriteLine(item);
+           
+            //if (userList[0] == computerList[0])
             //{
             //    Console.WriteLine("+");
             //}
 
-            //if (userList[2] == computerList[2])
+            //else if (userList[1] == computerList[1])
             //{
             //    Console.WriteLine("+");
             //}
 
-            //if (userList[3] == computerList[3])
+            //else if (userList[2] == computerList[2])
             //{
             //    Console.WriteLine("+");
             //}
 
+            //else if (userList[3] == computerList[3])
+            //{
+            //    Console.WriteLine("+");
+            //}
 
-
-
-
-
+            //else
+            //{
+            //    Console.WriteLine("-");
+            //}
         }
     }
 }
